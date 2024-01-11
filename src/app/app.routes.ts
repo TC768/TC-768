d011bbfa9938ee9b21aa94fabcb49e0d1858618b
Bryan Soto <bryangsu10@gmail.com>
2024-01-11 15:27:39 -0600

    Agregue nuevas rutas y actualice los estilos de la barra de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ResourceComponent } from './pages/resource/resource.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'aboutUs', component: AboutUsComponent}
+    {path: 'aboutUs', component: AboutUsComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'resources', component: ResourceComponent},
+    {path: '**', component: PageNotFoundComponent}
 ];
