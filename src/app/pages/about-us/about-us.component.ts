177444ad2df411448c2ac4367a18dcc866bc2009
Bryan Soto <37676815+buraiang99@users.noreply.github.com>
2024-01-17 16:28:54 -0600

    Revert "Feature/kc 09 sobre nosotros"

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { FooterComponent } from '../../components/footer/footer.component';
-import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { ApiService } from '../../core/services/api.service';
-import { EMPTY, Observable, catchError, tap } from 'rxjs';
-import { proyect } from '../../interfaces/proyect';
-import { AsyncPipe } from '@angular/common';
-import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [AsyncPipe, FooterComponent, NavbarComponent, ErrorMessageComponent],
+  imports: [],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
 
-  public proyects$!: Observable<proyect[]>;
-  public erroMessage: string = "";
-  constructor(private service: ApiService) { }
-
-  ngOnInit(): void {
-    this.proyects$ = this.service.getProyects().pipe(catchError((error: string) => {
-      this.erroMessage = error;
-      return EMPTY;
-    }));
-  }
 }
