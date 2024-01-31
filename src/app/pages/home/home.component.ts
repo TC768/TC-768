2d9146e1b56b25b0d55b1a6de224efd339c6fba6
Bryan Soto <bryangsu10@gmail.com>
2024-01-31 16:52:45 -0600

    Creacion componente card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  cardTitle: string = 'Titulo de la tarjeta';
+  iconPath: string = 'Software Testing Services.jpeg';
+  cardDescription: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.';
+
+  constructor() { }
 }
